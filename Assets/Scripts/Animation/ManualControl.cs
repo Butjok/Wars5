da70415bc17eecf58c91ced045e2df102e8528ce
Viktor Fedotov <butjok@gmail.com>
2022-08-26 20:35:13 +0200

    hffghhgfgfgh

--- Assets/Scripts/Animation/ManualControl.cs
+++ Assets/Scripts/Animation/ManualControl.cs
@@ -10,6 +10,8 @@ public class ManualControl:MonoBehaviour {
 		target = transform;
 	}
 	public void Update() {
+		if (!target)
+			return;
 		speed += Input.GetAxisRaw("Debug Vertical") * acceleration * Time.deltaTime;
 		speed = Mathf.Sign(speed) * Mathf.Min(maxSpeed,Mathf.Abs(speed));
 		target.rotation=Quaternion.Euler(0,target.rotation.eulerAngles.y+Input.GetAxisRaw("Debug Horizontal")*rotationSpeed*Time.deltaTime,0);
