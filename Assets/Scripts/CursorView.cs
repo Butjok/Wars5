8d3dcd5d4476c9221eea2d77768e79bbcb7309cd
Viktor Fedotov <butjok@gmail.com>
2022-09-07 23:48:01 +0200

    ролролролролрлрол

--- Assets/Scripts/CursorView.cs
+++ Assets/Scripts/CursorView.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 [RequireComponent(typeof(MeshRenderer))]
 public class CursorView : MonoBehaviour {
 	public MeshRenderer meshRenderer;
+	public bool show = true;
 	public void Reset() {
 		meshRenderer = GetComponent<MeshRenderer>();
 	}
 	public void LateUpdate() {
+		if (Input.GetKeyDown(KeyCode.F11)) 
+			show = !show;
 		if (Mouse.TryGetPosition(out Vector2Int position)) {
-			meshRenderer.enabled = true;
+			meshRenderer.enabled = show;
 			transform.position = position.ToVector3Int();
 		}
 		else
---
+++ Assets/Scripts/LevelState/DialogueState.cs
@@ -0,0 +1,8 @@
+public class DialogueState : LevelState {
+	
+	public DialogueState(Level level) : base(level) { }
+
+	public override void Dispose() {
+		base.Dispose();
+	}
+}
