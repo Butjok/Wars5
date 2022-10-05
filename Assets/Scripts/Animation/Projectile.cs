5f3e043aa2e25ea254dc4aa6bd1de590ede9a2f4
Viktor Fedotov <butjok@gmail.com>
2022-10-05 07:36:48 +0200

    lkdfjgoeriuteoirtu

--- Assets/Scripts/Animation/Projectile.cs
+++ Assets/Scripts/Animation/Projectile.cs
@@ -9,11 +9,18 @@ public class Projectile : MonoBehaviour {
 	public const float adjacentPositionDeltaTime = .1f;
 	public ParticleSystem impactPrefab;
 	public float impactForce = 500;
+	public bool destroy;
 
 	public void Update() {
 
-		if (time >= ballisticCurve.totalTime) {
+		if (destroy)
+			Destroy(gameObject);
 
+		else if (ballisticCurve.totalTime is {} totalTime && time >= totalTime) {
+
+			destroy = true;
+			transform.position = ballisticCurve.Sample(totalTime);
+			
 			foreach (var impactPoint in impactPoints) {
 
 				if (!impactPoint)
@@ -27,14 +34,13 @@ public class Projectile : MonoBehaviour {
 				if (impactPoint.unitView && impactPoint.unitView.bodyTorque)
 					impactPoint.unitView.bodyTorque.AddWorldForceTorque(impactPoint.transform.position, -impactPoint.transform.forward * impactForce);
 			}
-
-			Destroy(gameObject);
-			return;
 		}
 
-		transform.position = ballisticCurve.Sample(time);
-		var adjacent = ballisticCurve.Sample(time + adjacentPositionDeltaTime);
-		transform.rotation = Quaternion.LookRotation(adjacent - transform.position, Vector3.up);
-		time += Time.deltaTime;
+		else {
+			transform.position = ballisticCurve.Sample(time);
+			var adjacent = ballisticCurve.Sample(time + adjacentPositionDeltaTime);
+			transform.rotation = Quaternion.LookRotation(adjacent - transform.position, Vector3.up);
+			time += Time.deltaTime;
+		}
 	}
 }
