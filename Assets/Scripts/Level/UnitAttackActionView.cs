0ed24c1d008e5edefd883197364c89fce4488d65
Viktor Fedotov <butjok@gmail.com>
2022-12-14 09:17:41 +0100

    lkjsdlkfsd

--- Assets/Scripts/Level/UnitAttackActionView.cs
+++ Assets/Scripts/Level/UnitAttackActionView.cs
@@ -1,3 +1,4 @@
+using System;
 using Butjok.CommandLine;
 using TMPro;
 using UnityEngine;
@@ -7,14 +8,14 @@ using UnityEngine.Assertions;
 public class UnitAttackActionView : MonoBehaviour {
 
     public static UnitAttackActionView Prefab => nameof(UnitAttackActionView).LoadAs<UnitAttackActionView>();
-    
+
     public UnitAction action;
     public LineRenderer lineRenderer;
     public RectTransform uiRoot;
     public UIFrame uiFrame;
     public TMP_Text uiText;
     public string uiTextFormat = "−{0}";
-    public float targetRadius = .33f;
+    public float radius = .33f;
 
     private void Reset() {
         lineRenderer = GetComponent<LineRenderer>();
@@ -22,26 +23,38 @@ public class UnitAttackActionView : MonoBehaviour {
         lineRenderer.enabled = false;
     }
     private void Update() {
+
         if (lineRenderer.enabled) {
-            if (lineRenderer.positionCount != 2)
-                lineRenderer.positionCount = 2;
-            lineRenderer.SetPosition(0, action.unit.view.turret.computer.barrel.position);
-            lineRenderer.SetPosition(1, action.targetUnit.view.center.position);
+
+            var source = action.unit.view.center.position;
+            var destination = action.targetUnit.view.center.position;
+            if (Vector3.Distance(source, destination) < 2 * radius) {
+                lineRenderer.positionCount = 0;
+                return;
+            }
+
+            var direction = (destination - source).normalized;
+            lineRenderer.positionCount = 2;
+            lineRenderer.SetPosition(0, source + direction * radius);
+            lineRenderer.SetPosition(1, destination - direction * radius);
         }
     }
+
     [Command]
     public bool Show {
         set {
+
             action.unit.view.turret.aim = value;
             uiRoot.gameObject.SetActive(value);
             lineRenderer.enabled = value;
+
             if (!value)
                 return;
-            
+
             action.unit.view.turret.computer.Target = action.targetUnit.view.body.transform;
-            
+
             uiFrame.boxCollider = action.targetUnit.view.uiBoxCollider;
-            
+
             var (_, newTargetHp) = action.CalculateHpsAfterAttack();
             var difference = action.targetUnit.hp.v - newTargetHp;
             uiText.text = string.Format(uiTextFormat, difference);
