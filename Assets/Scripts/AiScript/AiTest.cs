770f7b7bf4b86c710bb023d94a044203b1f9c938
Viktor Fedotov <butjok@gmail.com>
2022-12-20 05:49:50 +0100

    nbmnbmbmn

--- Assets/Scripts/AiScript/AiTest.cs
+++ Assets/Scripts/AiScript/AiTest.cs
@@ -1,19 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
 using NaughtyAttributes;
+using RoslynCSharp;
 using UnityEngine;
+using UnityEngine.Assertions;
+
 
 public class AiTest : MonoBehaviour {
     
     public Transform[] transforms = { };
     
     [ResizableTextArea] public string code = @"
+using System.Collections.Generic;
 using UnityEngine;
 
-public class AiTestProcessing {{
-    public static const int pi = 4;
-}}";
+public class AiTestProcessing {
+    public void Execute(Unit unit) {
+         
+    }
+}";
+
+    public Game game;
+    
+    private void Start() {
+        game = Testing.CreateGame();
+    }
     
     [Button]
     public void Execute() {
-        
+        var unit = game.units.Values.First();
+        var domain = ScriptDomain.CreateDomain("AiTesting");
+        var scriptType = domain.CompileAndLoadMainSource(code);
+        Assert.IsNotNull(scriptType);
+        var proxy = scriptType.CreateInstance();
+        proxy.Call("Execute", unit);
     }
 }
