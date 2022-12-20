ff227c94d1a7194d12e8b6a70246fbbb4429b26e
Viktor Fedotov <butjok@gmail.com>
2022-12-20 06:09:58 +0100

    tyuyuttyu

--- Assets/Scripts/AiBrain.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class AiBrain : MonoBehaviour {
-
-    [TextArea(10,50)]
-    public string code = "";
-    
-    
-}
---
+++ Assets/Scripts/UnitBrain.cs
@@ -0,0 +1,36 @@
+using System;
+using NaughtyAttributes;
+using RoslynCSharp;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+[RequireComponent(typeof(UnitView))]
+public class UnitBrain : MonoBehaviour {
+
+    [ResizableTextArea] public string code = @"
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AiTestProcessing {
+    public static UnitAction FindAction(Unit unit) {
+         return null;
+    }
+}";
+
+    public ScriptDomain domain;
+
+    private void Start() {
+        domain = ScriptDomain.CreateDomain("AiTest");
+    }
+
+    public UnitAction FindAction() {
+        var scriptType = domain.CompileAndLoadMainSource(code);
+        Assert.IsNotNull(scriptType);
+        return (UnitAction)scriptType.CallStatic("FindAction", GetComponent<UnitView>().unit);
+    }
+
+    [Button]
+    public void Trigger() {
+        var action = FindAction();
+    }
+}
