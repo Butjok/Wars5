b66ab96686a3518ff512c9d4d417887716fec669
Viktor Fedotov <butjok@gmail.com>
2023-03-13 03:37:54 +0100

    small bug in camera rig jump

--- Assets/Scripts/CameraRig.cs
+++ Assets/Scripts/CameraRig.cs
@@ -217,6 +217,7 @@ public class CameraRig : MonoBehaviour {
             yield return null;
         }
         transform.position = to;
+        JumpCoroutine = null;
     }
 
     public float PitchAngle {
