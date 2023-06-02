a91e3d3ed8c96b6663aa5a4e519c463cf10a1ea1
Viktor Fedotov <butjok@gmail.com>
2023-06-02 19:26:31 +0200

    added image and video in dialogue

--- Assets/Scripts/DialogueUi3.cs
+++ Assets/Scripts/DialogueUi3.cs
@@ -7,14 +7,16 @@ using JetBrains.Annotations;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Assertions;
+using UnityEngine.UI;
+using UnityEngine.Video;
 using static DialogueConstants;
 using static Gettext;
+using Object = UnityEngine.Object;
 
 /*
  * TODO:
  * 1) Add a way to change the speaker's name position.
  * 2) Add speech shaking e.g. because of explosion impact.
- * 3) Being able to display images and videos anywhere on the screen.
  */
 
 public class DialogueUi3 : MonoBehaviour {
@@ -34,6 +36,8 @@ public class DialogueUi3 : MonoBehaviour {
     public TMP_Text text;
     public PortraitStack[] portraitStacks = { };
     private AudioSource voiceOverSource, sfxSource;
+    public Button buttonPrefab;
+    public float buttonSpacing = 25;
 
     public bool Visible {
         set => gameObject.SetActive(value);
@@ -68,6 +72,7 @@ public class DialogueState : IDisposableState {
     public DialogueUi3 ui;
     public Dictionary<PersonName, PortraitStack> portraitStacks = new();
     public DialogueContext dialogueContext = new();
+    public int? option;
 
     public DialogueState(DialogueUi3 ui) {
         Assert.IsTrue(ui);
@@ -115,19 +120,53 @@ public class DialogueState : IDisposableState {
             Speaker = null;
 
             yield return AddPerson(PersonName.Natalie);
-            using (LinesOf(PersonName.Natalie)) {
-                yield return Say(_("Hello, my name is Natalie."));
-                yield return AddPerson(PersonName.Vladan, right);
-                using (LinesOf(PersonName.Vladan)) {
-                    yield return Say(_("Hello, my name is Vladan."));
+            // using (LinesOf(PersonName.Natalie)) {
+            //     yield return Say(_("Hello, my name is Natalie."));
+            //     yield return AddPerson(PersonName.Vladan, right);
+            //     using (LinesOf(PersonName.Vladan)) {
+            //         yield return Say(_("Hello, my name is Vladan."));
+            //     }
+            //     yield return RemovePerson(PersonName.Vladan);
+            //     yield return Say(_("I am a student of the Faculty of Mathematics."));
+            // }
+            // yield return AddPerson(PersonName.Vladan, left);
+
+            // var video = ShowVideo("encoded2".LoadAs<VideoClip>(), new Vector2(500, 500));
+            // var completed = false;
+            // video.videoPlayer.loopPointReached += _ => completed = true;
+            // yield return WaitWhile(() => !completed);
+            // HideVideo(video);
+
+            Speaker = PersonName.Natalie;
+            while (option is not (0 or 1)) {
+                yield return SayContinue(_("Do you like apples?"));
+                yield return SelectOption(_("Yes"), _("No"), _("Explain"));
+                switch (option) {
+                    case 0:
+                        yield return Say(_("I like apples too!"));
+                        break;
+                    case 1:
+                        yield return Say(_("I don't like apples either."));
+                        break;
+                    case 2:
+                        var image = ShowImage("apple".LoadAs<Sprite>(), new Vector2(150, 150));
+                        yield return SayContinue(_("This is an apple."));
+                        yield return Pause(1);
+                        yield return AppendContinue(_("It is a fruit."));
+                        yield return Pause(1);
+                        yield return AppendContinue(_("It is red."));
+                        yield return Pause(1);
+                        yield return Append(_("It is tasty."));
+                        HideImage(image);
+                        break;
                 }
-                yield return RemovePerson(PersonName.Vladan);
-                yield return Say(_("I am a student of the Faculty of Mathematics."));
             }
-            yield return AddPerson(PersonName.Vladan, left);
-            using (LinesOf(PersonName.Vladan)) {
-                yield return Say(_("Hello, my name is Vladan."));
-            }
+
+            yield return Say(_("So we are done here. Goodbye!"));
+
+            Speaker = null;
+            Text = null;
+
             yield return ClearPersons();
 
             // yield return Say(PersonName.Natalie, _("Hello, my name is Natalie."));
@@ -189,43 +228,131 @@ public class DialogueState : IDisposableState {
         ui.SfxSource.PlayOneShot(audioClip);
     }
 
-    public DialogueSpeaker LinesOf(PersonName personName) {
-        return new DialogueSpeaker(dialogueContext, personName);
+    public StateChange Say(string text, bool waitAnyKeyDown = true, bool append=false) {
+        return StateChange.Push("Talk", TalkState(text, waitAnyKeyDown,append));
     }
-    public StateChange Say(PersonName personName, string text, bool wait = true, AudioClip voiceOverClip = null) {
-        return StateChange.Push($"{personName} talks", TalkState(personName, text, wait, voiceOverClip));
+    public StateChange SayContinue(string text) {
+        return Say(text, false, false);
     }
-    public StateChange Say(string text, bool wait = true, AudioClip voiceOverClip = null) {
-        var personName = dialogueContext.personStack.Peek();
-        return StateChange.Push($"{personName} talks", TalkState(personName, text, wait, voiceOverClip));
+    public StateChange Append(string text) {
+        return Say(text, true, true);
     }
-    public IEnumerator<StateChange> TalkState(PersonName personName, string text, bool wait = true, AudioClip voiceOverClip = null) {
+    public StateChange AppendContinue(string text) {
+        return Say(text, false, true);
+    }
+    
+    public IEnumerator<StateChange> TalkState(string text, bool wait = true, bool append=false) {
 
-        Speaker = personName;
-        Text = "";
-
+        if (append)
+            text = " " + text;
+        else
+            Text = "";
+        
         ui.VoiceOverSource.Stop();
-        if (voiceOverClip)
-            ui.VoiceOverSource.PlayOneShot(voiceOverClip);
+        // if (voiceOverClip)
+        //     ui.VoiceOverSource.PlayOneShot(voiceOverClip);
 
         var from = Text;
-        var to = from + text;
         foreach (var c in text) {
             Text += c;
-            if (Input.anyKeyDown)
-                Text = to;
+            if (Input.anyKeyDown) {
+                Text = from + text;
+                yield return StateChange.none;
+                break;
+            }
             yield return StateChange.none;
         }
 
         ui.VoiceOverSource.Stop();
 
-        if (wait) {
-            while (!Input.anyKeyDown)
-                yield return StateChange.none;
+        if (wait)
+            yield return WaitUntilAnyKeyDown();
+    }
+
+    public Image ShowImage(Sprite sprite, Vector2? size = null, Vector2 position = default) {
+        var go = new GameObject($"Image{sprite}");
+        go.transform.SetParent(ui.transform);
+        var image = go.AddComponent<Image>();
+        image.sprite = sprite;
+        image.rectTransform.sizeDelta = size ?? new Vector2(sprite.texture.width, sprite.texture.height);
+        image.rectTransform.anchoredPosition = position;
+        image.preserveAspect = true;
+        return image;
+    }
+    public void HideImage(Image image) {
+        Object.Destroy(image.gameObject);
+    }
+
+    public (VideoPlayer videoPlayer, RawImage image) ShowVideo(VideoClip videoClip, Vector2 size, Vector2 position = default) {
+
+        var go = new GameObject($"Video{videoClip}");
+        go.transform.SetParent(ui.transform);
+
+        var videoPlayer = go.AddComponent<VideoPlayer>();
+        videoPlayer.clip = videoClip;
+        videoPlayer.renderMode = VideoRenderMode.RenderTexture;
+        var renderTexture = new RenderTexture((int)videoClip.width, (int)videoClip.height, 0);
+        videoPlayer.targetTexture = renderTexture;
+        videoPlayer.transform.SetParent(ui.transform);
+        videoPlayer.Play();
+
+        var rawImage = go.AddComponent<RawImage>();
+        rawImage.texture = renderTexture;
+        // fits horizontally
+        rawImage.rectTransform.sizeDelta = new Vector2(size.x, size.x * ((float)videoClip.height / videoClip.width));
+        rawImage.rectTransform.anchoredPosition = position;
+
+        return (videoPlayer, rawImage);
+    }
+    public void HideVideo((VideoPlayer videoPlayer, RawImage rawImage) video) {
+        Object.Destroy(video.videoPlayer.gameObject);
+    }
+
+    public StateChange WaitWhile(Func<bool> condition) {
+        return StateChange.Push(nameof(WaitWhile), WaitWhileState(condition));
+    }
+    public IEnumerator<StateChange> WaitWhileState(Func<bool> condition) {
+        while (condition())
             yield return StateChange.none;
+    }
+
+    public StateChange WaitUntilAnyKeyDown() {
+        return WaitWhile(() => !Input.anyKeyDown);
+    }
+
+    public StateChange SelectOption(params string[] options) {
+        return StateChange.Push(nameof(OptionSelectionState), OptionSelectionState(options));
+    }
+    public IEnumerator<StateChange> OptionSelectionState(params string[] options) {
+
+        Assert.IsTrue(options.Length > 0);
+
+        var buttons = new List<Button>();
+        var totalWidth = 0f;
+        for (var i = 0; i < options.Length; i++) {
+            var index = i;
+            var button = Object.Instantiate(ui.buttonPrefab, ui.transform);
+            button.gameObject.SetActive(true);
+            button.GetComponentInChildren<TMP_Text>().text = options[index];
+            button.onClick.AddListener(() => option = index);
+            buttons.Add(button);
+            totalWidth += button.GetComponent<RectTransform>().sizeDelta.x;
         }
-        Text = null;
-        Speaker = null;
+        totalWidth += ui.buttonSpacing * (buttons.Count - 1);
+        Vector2 position = ui.buttonPrefab.GetComponent<RectTransform>().anchoredPosition;
+        position -= Vector2.right * totalWidth / 2;
+        foreach (var button in buttons) {
+            var rectTransform = button.GetComponent<RectTransform>();
+            rectTransform.anchoredPosition = position;
+            position += Vector2.right * (rectTransform.sizeDelta.x + ui.buttonSpacing);
+        }
+
+        option = null;
+        while (option == null)
+            yield return StateChange.none;
+
+        foreach (var button in buttons)
+            Object.Destroy(button.gameObject);
     }
 }
 
