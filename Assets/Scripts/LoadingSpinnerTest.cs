60bb866472b5543e5299ed7696c45cc0e339ea8e
Viktor Fedotov <butjok@gmail.com>
2023-06-03 21:33:10 +0200

    asdasd

---
+++ Assets/Scripts/LoadingSpinnerTest.cs
@@ -0,0 +1,17 @@
+using System;
+using Butjok.CommandLine;
+using Drawing;
+using UnityEngine;
+
+public class LoadingSpinnerTest : MonoBehaviour {
+    
+    public Transform start, finish;
+    [Command]
+    public float targetProgress;
+
+    public float Progress => (transform.position - start.position).magnitude / (finish.position - start.position).magnitude;
+
+    private void OnGUI() {
+        GUILayout.Label($"Progress: {Progress}");
+    }
+}
---
+++ Assets/Scripts/MainMenuUnitLoop.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class MainMenuUnitLoop : MonoBehaviour {
+
+    public float length = 1;
+    public Transform[] actors = { };
+    public float speed = 1;
+
+    public Vector3 LineStart => transform.position;
+    public Vector3 LineEnd => transform.position + LineDirection * length;
+    public Vector3 LineDirection => transform.forward;
+
+    public static Vector3 GetClosestPointOnLineSegment(Vector3 lineStart, Vector3 lineEnd, Vector3 point) {
+        var lineDirection = lineEnd - lineStart;
+        var lineMagnitude = lineDirection.magnitude;
+        lineDirection.Normalize();
+
+        var closestPointDistance = Vector3.Dot((point - lineStart), lineDirection);
+        closestPointDistance = Mathf.Clamp(closestPointDistance, 0f, lineMagnitude);
+
+        return lineStart + lineDirection * closestPointDistance;
+    }
+
+    public void Update() {
+        foreach (var actor in actors) {
+            var closestPoint = GetClosestPointOnLineSegment(LineStart, LineEnd, actor.position);
+            var distance = (closestPoint - LineStart).magnitude;
+            var nextDistance = distance + speed * Time.deltaTime;
+            var oldPosition = actor.position;
+            var nextPosition = LineStart + LineDirection * nextDistance;
+            actor.rotation = Quaternion.LookRotation(nextPosition - oldPosition, transform.up);
+            var wrappedDistance = nextDistance % length;
+            actor.position = LineStart + LineDirection * wrappedDistance;
+        }
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(LineStart, LineEnd);
+    }
+}
