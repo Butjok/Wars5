2f095c1b5e6b632572fa6c752238e533df47c559
Viktor Fedotov <butjok@gmail.com>
2023-09-25 11:11:24 +0200

    sdfsdfsdf

--- Assets/Scripts/PropPlacement.cs
+++ Assets/Scripts/PropPlacement.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Drawing;
 using UnityEngine;
+using UnityEngine.Assertions;
 using Random = UnityEngine.Random;
 
 public class PropPlacement : MonoBehaviour {
@@ -15,10 +19,12 @@ public class PropPlacement : MonoBehaviour {
     public List<Transform> prefabs = new();
     public Transform prefab;
     public Dictionary<Transform, Transform> previews = new();
+    public Dictionary<Transform, Transform> getPrefab = new();
 
     private void Awake() {
         if (prefabs.Count > 0)
             prefab = prefabs[0];
+        TryLoad();
     }
 
     public void Update() {
@@ -52,12 +58,8 @@ public class PropPlacement : MonoBehaviour {
                     props.Remove(closestProp);
                     Destroy(closestProp.gameObject);
                 }
-                else if (prefab) {
-                    var prop = Instantiate(prefab);
-                    prop.position = hit.point;
-                    prop.rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up) * Quaternion.Euler(0, Random.value * 360, 0);
-                    props.Add(prop);
-                }
+                else if (prefab)
+                    AddProp(prefab, hit.point, Quaternion.LookRotation(Vector3.forward, hit.normal) * Quaternion.Euler(0, Random.value * 360, 0), Vector3.one);
             }
         }
 
@@ -68,7 +70,53 @@ public class PropPlacement : MonoBehaviour {
         }
     }
 
-    public void Save(string path = savePath) { }
+    public Transform AddProp(Transform prefab, Vector3 position, Quaternion rotation, Vector3 scale) {
+        var prop = Instantiate(prefab);
+        prop.position = position;
+        prop.rotation = rotation;
+        prop.localScale = scale;
+        props.Add(prop);
+        getPrefab.Add(prop, prefab);
+        return prop;
+    }
 
-    public void TryLoad(string path = savePath) { }
+    private void OnGUI() {
+        GUI.skin = DefaultGuiSkin.TryGet;
+        if (prefab)
+            GUILayout.Label($"Prop: {prefab.name}");
+    }
+
+    private void OnApplicationQuit() {
+        Save();
+    }
+
+    public void Save(string path = savePath) {
+        var writer = new StringWriter();
+        foreach (var prop in props)
+            writer.PostfixWriteLine("add ( {0} {1} {2} {3} )", getPrefab[prop].name, prop.position, prop.rotation, prop.localScale);
+        File.WriteAllText(path, writer.ToString());
+    }
+
+    public bool TryLoad(string path = savePath) {
+        if (!File.Exists(path))
+            return false;
+        var stack = new Stack();
+        foreach (var token in Tokenizer.Tokenize(File.ReadAllText(path).ToPostfix()))
+            switch (token) {
+                case "add": {
+                    var scale = (Vector3)stack.Pop();
+                    var rotation = (Quaternion)stack.Pop();
+                    var position = (Vector3)stack.Pop();
+                    var prefabName = (string)stack.Pop();
+                    var prefabs = this.prefabs.Where(p => p.name == prefabName).ToList();
+                    Assert.IsTrue(prefabs.Count == 1);
+                    AddProp(prefabs[0], position, rotation, scale);
+                    break;
+                }
+                default:
+                    stack.ExecuteToken(token);
+                    break;
+            }
+        return true;
+    }
 }
